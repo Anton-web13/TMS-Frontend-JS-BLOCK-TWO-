15e23bd72de786cd0405edd4b7c90e3d89a60b83
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2023-01-17 12:47:02 +0300

    homework-14 1 Task

--- Homework/js/homework-14.js
+++ Homework/js/homework-14.js
@@ -11,51 +11,6 @@ const lettersToEven = (letters, size) => {
     return subarray
 };
 
-// console.log(lettersToEven(letters, 3));
-
-// 2)
-const solutions = async () => {
-
-    try {
-
-        const result = [];
-
-        const getPosts = await fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-
-        const getUsers  = await fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-
-        // console.log(getUsers);
-
-        console.log(getPosts);
-
-        // for (const post of getPosts) {
-        //
-        //     const getUser  = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-        //         .then(response => response.json())
-        //
-        //     result.push(getUser)
-        // }
-
-        for (const user of getUsers) {
-
-            // console.log(user.id)
-
-            const getPosts = await fetch(`https://jsonplaceholder.typicode.com/posts/${user.id}`)
-                .then(response => response.json())
-
-            result.push(getPosts)
-        }
-
-        console.log(result)
-
-    }
-    catch (error) {
-        console.log(error.message)
-    }
-};
-
-console.log(solutions());
+console.log(lettersToEven(letters, 3));
 
 
